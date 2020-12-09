6eba49b26160e49361a71f6b4e5c02c51e84a71d
iagan94 <l.carturan@gmail.com>
2020-12-09 02:08:55 +0100

    close #2 Aggiornata funzione per applicare sconto 50% su ordini con piu' di 5 gelati

--- src/main/java/it/unipd/tos/business/TakeAway.java
+++ src/main/java/it/unipd/tos/business/TakeAway.java
@@ -16,11 +16,31 @@ public class TakeAway implements TakeAwayBill {
     @Override
     public double getOrderPrice(List<MenuItem> itemsOrdered, User user, LocalTime time) throws TakeAwayBillException {
         float total = 0;
+        int nGelati = 0;
 
         for(MenuItem item : itemsOrdered) {
             total += item.getPrice();
+            if(item.getType().equals(MenuItem.itemType.Gelato)) {
+                nGelati++;
+            }
         }
-        return total;
+        if(nGelati > 5) {
+            return total - scontoGelatoMenoCaro(itemsOrdered);
+        } else {
+            return total;
+        }
+    }
+
+    private double scontoGelatoMenoCaro(List<MenuItem> itemsOrdered) {
+        double sconto = Double.MAX_VALUE;
+        for(MenuItem item : itemsOrdered) {
+            if(item.getType().equals(MenuItem.itemType.Gelato)) {
+                if(item.getPrice() < sconto) {
+                    sconto = item.getPrice();
+                }
+            }
+        }
+        return sconto/2;
     }
 
 }
--- src/test/java/it/unipd/tos/business/TakeAwayTest.java
+++ src/test/java/it/unipd/tos/business/TakeAwayTest.java
@@ -22,18 +22,27 @@ public class TakeAwayTest {
 
 	private static final double DELTA = 1e-15;
 	
-	private List<MenuItem> list;
+	private List<MenuItem> listaSemplice;
+    private List<MenuItem> lista6Gelati;
 	private User user;
 	private LocalTime time;
 	private TakeAway order;
 	
 	@Before
 	public void setUp() {
-		list = new ArrayList<MenuItem>();
-		list.add(new MenuItem(MenuItem.itemType.Gelato, "Vaniglia", 3));
-		list.add(new MenuItem(MenuItem.itemType.Budino, "Pinguino", 5.5));
-		list.add(new MenuItem(MenuItem.itemType.Bevanda, "Aranciata", 2.5));
-		user = new User("Mario", "Rossi", 30);
+        listaSemplice = new ArrayList<MenuItem>();
+        listaSemplice.add(new MenuItem(MenuItem.itemType.Gelato, "Vaniglia", 3));
+        listaSemplice.add(new MenuItem(MenuItem.itemType.Budino, "Pinguino", 5.5));
+        listaSemplice.add(new MenuItem(MenuItem.itemType.Bevanda, "Aranciata", 2.5));
+
+        lista6Gelati = new ArrayList<MenuItem>(listaSemplice);
+        lista6Gelati.add(new MenuItem(MenuItem.itemType.Gelato, "Cioccolato", 3));
+        lista6Gelati.add(new MenuItem(MenuItem.itemType.Gelato, "Tre Gusti", 5));
+        lista6Gelati.add(new MenuItem(MenuItem.itemType.Gelato, "Bacio", 4));
+        lista6Gelati.add(new MenuItem(MenuItem.itemType.Gelato, "Mignon", 2));
+        lista6Gelati.add(new MenuItem(MenuItem.itemType.Gelato, "Tiramisu'", 4));
+
+        user = new User("Mario", "Rossi", 30);
 		time = LocalTime.of(18, 0);
 		order = new TakeAway();
 		
@@ -42,11 +51,21 @@ public class TakeAwayTest {
 	@Test
 	public void getOrderPriceTest() throws TakeAwayBillException {
 		try {
-			double tot = order.getOrderPrice(list, user, time);
+			double tot = order.getOrderPrice(listaSemplice, user, time);
 			assertEquals(11, tot, DELTA);
 		} catch(TakeAwayBillException e) {
 			//
 		}
 	}
+	
+    @Test
+    public void getOrderPriceTestPiuDi5Gelati() throws TakeAwayBillException {
+        try {
+            double tot = order.getOrderPrice(lista6Gelati, user, time);
+            assertEquals(28, tot, DELTA);
+        } catch(TakeAwayBillException e) {
+        //
+        }
+    }
 
 }
